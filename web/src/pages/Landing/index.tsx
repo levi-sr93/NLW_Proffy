0ed702498ec3f1d297cafac366aaf8a32d299560
levi-sr93 <levi-sr@outlook.com>
2020-10-05 16:59:32 -0300

    Styling for desktop and medium devices

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -16,11 +16,12 @@ const Landing = () => {
 
          <div className="logo-container">
            <img src={logoImg} alt="Proffy"/>
-         </div>
            <h2>Your online study platform.</h2>
+         </div>
+
 
          <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
-
+         
         <div className="buttons-container">
           <a href="" className="study">
             <img src={studyIcon} alt="Estudar"/>
