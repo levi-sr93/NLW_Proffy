f14a1cc388953f72bea22b20eaafe901de594af1
Levi Rocha <levi.rocha@teclat.com.br>
2020-10-05 15:39:13 -0300

    Initial Structure|Layout|GlobalCSS|Landing Style

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
+
+
+const Landing = () => {
+  return (
+    <div id="page-landing">
+       <div id="page-landing-content" className="container">
+
+         <div className="logo-container">
+           <img src={logoImg} alt="Proffy"/>
+         </div>
+           <h2>Your online study platform.</h2>
+
+         <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
+
+        <div className="buttons-container">
+          <a href="" className="study">
+            <img src={studyIcon} alt="Estudar"/>
+            Study
+          </a>
+
+          <a href="" className="teach">
+            <img src={giveClassIcon} alt="Estudar"/>
+            Teach
+          </a>
+        </div>
+
+        <span className="total-connections">Total of 200 conections  <img src={purpleHeartIcon} alt="purple heart"/></span>
+
+       </div>
+    </div>
+  )
+}
+
+export default Landing;
