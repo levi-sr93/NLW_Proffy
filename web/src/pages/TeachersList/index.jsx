59262a9c07cb3fd58f10ab42647fdefcb9f92de7
Levi Santos <levi-sr@outlook.com>
2020-10-05 22:26:37 -0300

    Creating the Page Header Component

---
+++ web/src/pages/TeachersList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+const TeachersList = () => {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Here is all the Proffys available" />
+    </div>
+  );
+};
+
+export default TeachersList;
